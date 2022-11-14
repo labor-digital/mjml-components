49337d88fdd1bd47cf4c159a532de59e683d4792
Arni <toarni@gmail.com>
2022-11-14 15:11:18 +0100

    chore: refactor identation

--- src/Adobe/EdEx/LaborAdobeEdexTwoColArticle.js
+++ src/Adobe/EdEx/LaborAdobeEdexTwoColArticle.js
@@ -1,40 +1,40 @@
-import { registerDependencies } from 'mjml-validator'
-import { BodyComponent } from 'mjml-core'
+import {registerDependencies} from 'mjml-validator'
+import {BodyComponent} from 'mjml-core'
 import AdobeProductMapping from '../AdobeProductMapping'
 
 registerDependencies({
-  'mj-column': ['labor-adobe-edex-two-col-article'],
-  'labor-adobe-edex-two-col-article': [],
+    'mj-column': ['labor-adobe-edex-two-col-article'],
+    'labor-adobe-edex-two-col-article': [],
 })
 
 export default class LaborAdobeEdexTwoColArticle extends BodyComponent {
-  static endingTag = true
+    static endingTag = true
 
-  static allowedAttributes = {
-    '1-image-src': 'string',
-    '1-image-src-mobile': 'string',
-    '1-href': 'string',
-    '1-headline': 'string',
-    '1-category': 'string',
-    '1-cta': 'string',
-    '1-text': 'string',
-    '1-product': 'string',
-    '2-image-src': 'string',
-    '2-image-src-mobile': 'string',
-    '2-href': 'string',
-    '2-headline': 'string',
-    '2-category': 'string',
-    '2-cta': 'string',
-    '2-text': 'string',
-    '2-product': 'string',
-  }
+    static allowedAttributes = {
+        '1-image-src': 'string',
+        '1-image-src-mobile': 'string',
+        '1-href': 'string',
+        '1-headline': 'string',
+        '1-category': 'string',
+        '1-cta': 'string',
+        '1-text': 'string',
+        '1-product': 'string',
+        '2-image-src': 'string',
+        '2-image-src-mobile': 'string',
+        '2-href': 'string',
+        '2-headline': 'string',
+        '2-category': 'string',
+        '2-cta': 'string',
+        '2-text': 'string',
+        '2-product': 'string',
+    }
 
-  static defaultAttributes = {
-    '1-product': '',
-    '2-product': '',
-  }
+    static defaultAttributes = {
+        '1-product': '',
+        '2-product': '',
+    }
 
-  headStyle = (breakpoint) => `
+    headStyle = (breakpoint) => `
         .laborAdobeEdexTwoColArticle-first-elem {
             padding-left: 50px !important;
             padding-right: 15px !important;
@@ -78,8 +78,8 @@ export default class LaborAdobeEdexTwoColArticle extends BodyComponent {
         }
     `
 
-  render() {
-    return this.renderMJML(`
+    render() {
+        return this.renderMJML(`
             <labor-adobe-section with-padding="false">
                 <mj-column css-class="laborAdobeEdexTwoColArticle-first" padding-right="2px" padding-bottom="100px">
                     <labor-responsive-image
@@ -94,35 +94,35 @@ export default class LaborAdobeEdexTwoColArticle extends BodyComponent {
                         alt="${this.getAttribute('1-headline')}"
                     />
                     ${
-                      this.getAttribute('1-product')
-                        ? `
+            this.getAttribute('1-product')
+                ? `
                         <mj-image
                             src="${
-                              AdobeProductMapping.productMapping[this.getAttribute('1-product')][
-                                'images'
-                              ]['black']
-                            }"
+                    AdobeProductMapping.productMapping[this.getAttribute('1-product')][
+                        'images'
+                        ]['black']
+                }"
                             align="left"
                             width="${Math.floor(
-                              parseInt(
-                                AdobeProductMapping.productMapping[this.getAttribute('1-product')][
-                                  'width'
-                                ].replace('px', '')
-                              ) / 2
-                            )}px"
+                    parseInt(
+                        AdobeProductMapping.productMapping[this.getAttribute('1-product')][
+                            'width'
+                            ].replace('px', '')
+                    ) / 2
+                )}px"
                             height="30px"
                             target="_blank"
                             padding-bottom="24px"
                             css-class="laborAdobeEdexTwoColArticle-first-elem-product"
                             alt="${
-                              AdobeProductMapping.productMapping[this.getAttribute('1-product')][
-                                'name'
-                              ]
-                            }"
+                    AdobeProductMapping.productMapping[this.getAttribute('1-product')][
+                        'name'
+                        ]
+                }"
                         />
                     `
-                        : ``
-                    }
+                : ``
+        }
                     <labor-adobe-typo-body css-class="laborAdobeEdexTwoColArticle-first-elem" padding-bottom="7px">
                         ${this.getAttribute('1-category')}
                     </labor-adobe-typo-body>
@@ -133,8 +133,8 @@ export default class LaborAdobeEdexTwoColArticle extends BodyComponent {
                         ${this.getAttribute('1-text')}
                     </labor-adobe-typo-body>
                     <labor-adobe-secondary-cta css-class="laborAdobeEdexTwoColArticle-first-elem" type="quiet" href="${this.getAttribute(
-                      '1-href'
-                    )}">
+            '1-href'
+        )}">
                         ${this.getAttribute('1-cta')}
                     </labor-adobe-secondary-cta>
                 </mj-column>
@@ -151,35 +151,35 @@ export default class LaborAdobeEdexTwoColArticle extends BodyComponent {
                         alt="${this.getAttribute('2-headline')}"
                     />
                     ${
-                      this.getAttribute('2-product')
-                        ? `
+            this.getAttribute('2-product')
+                ? `
                         <mj-image
                             src="${
-                              AdobeProductMapping.productMapping[this.getAttribute('2-product')][
-                                'images'
-                              ]['black']
-                            }"
+                    AdobeProductMapping.productMapping[this.getAttribute('2-product')][
+                        'images'
+                        ]['black']
+                }"
                             align="left"
                             width="${Math.floor(
-                              parseInt(
-                                AdobeProductMapping.productMapping[this.getAttribute('2-product')][
-                                  'width'
-                                ].replace('px', '')
-                              ) / 2
-                            )}px"
+                    parseInt(
+                        AdobeProductMapping.productMapping[this.getAttribute('2-product')][
+                            'width'
+                            ].replace('px', '')
+                    ) / 2
+                )}px"
                             height="30px"
                             target="_blank"
                             padding-bottom="24px"
                             css-class="laborAdobeEdexTwoColArticle-second-elem-product"
                             alt="${
-                              AdobeProductMapping.productMapping[this.getAttribute('2-product')][
-                                'name'
-                              ]
-                            }"
+                    AdobeProductMapping.productMapping[this.getAttribute('2-product')][
+                        'name'
+                        ]
+                }"
                         />
                     `
-                        : ``
-                    }
+                : ``
+        }
                     <labor-adobe-typo-body css-class="laborAdobeEdexTwoColArticle-second-elem" padding-bottom="7px">
                         ${this.getAttribute('2-category')}
                     </labor-adobe-typo-body>
@@ -190,12 +190,12 @@ export default class LaborAdobeEdexTwoColArticle extends BodyComponent {
                         ${this.getAttribute('2-text')}
                     </labor-adobe-typo-body>
                     <labor-adobe-secondary-cta css-class="laborAdobeEdexTwoColArticle-second-elem" type="quiet" href="${this.getAttribute(
-                      '2-href'
-                    )}">
+            '2-href'
+        )}">
                         ${this.getAttribute('2-cta')}
                     </labor-adobe-secondary-cta>
                 </mj-column>
             </labor-adobe-section>
         `)
-  }
+    }
 }
