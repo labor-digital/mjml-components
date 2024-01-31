7496169bc500c76c167dbc328018cfd6c99498a7
Philipp Klingemeier <p.klingemeier@labor.digital>
2024-01-31 12:27:48 +0100

    fix: update footer band

---
+++ src/Adobe/_Base/LaborAdobeFooterBand.js
@@ -0,0 +1,52 @@
+import {registerDependencies} from 'mjml-validator'
+import {BodyComponent} from 'mjml-core'
+
+registerDependencies({
+    'mj-body': ['labor-adobe-footer-band'],
+    'labor-adobe-footer-band': [],
+})
+
+export default class LaborAdobeFooterBand extends BodyComponent {
+    static endingTag = true
+
+    static allowedAttributes = {
+        'bg-color': 'string',
+        'color': 'string',
+        'padding-top': 'unit(px,%)',
+        'padding-bottom': 'unit(px,%)'
+    }
+
+    static defaultAttributes = {
+        'bg-color': '#fa0f00',
+        'color': '#ffffff',
+        'font-size': '16px',
+        'font-weight': 800,
+        'line-height': '20px',
+        'align': 'left',
+        'padding-top': '5px',
+        'padding-bottom': '5px'
+    }
+
+    render() {
+
+        let typoAttrs = {
+            'color': this.getAttribute('color'),
+            'font-size': this.getAttribute('font-size'),
+            'font-weight': this.getAttribute('font-weight'),
+            'line-height': this.getAttribute('line-height'),
+            'align': this.getAttribute('align'),
+            'padding-top': this.getAttribute('padding-top'),
+            'padding-bottom': this.getAttribute('padding-bottom'),
+        }
+
+        return this.renderMJML(`
+            <labor-adobe-section background-color="${this.getAttribute('bg-color')}">
+                <mj-column>
+                    <mj-text ${this.htmlAttributes(typoAttrs)} >
+                        ${this.getContent()}
+                    </mj-text>
+                </mj-column>
+            </labor-adobe-section>
+        `)
+    }
+}
--- src/Adobe/_Base/LaborAdobeFooterRedBand.js
+++ src/Adobe/_Base/LaborAdobeFooterRedBand.js
@@ -1,6 +1,5 @@
 import {registerDependencies} from 'mjml-validator'
 import {BodyComponent} from 'mjml-core'
-import LaborAdobeSection from './LaborAdobeSection'
 
 registerDependencies({
     'mj-body': ['labor-adobe-footer-red-band'],
@@ -11,12 +10,6 @@ export default class LaborAdobeFooterRedBand extends BodyComponent {
     static endingTag = true
 
     render() {
-        return this.renderMJML(`
-            <labor-adobe-section background-color="#fa0f00">
-                <mj-column>
-                    <labor-adobe-typo-headingfive padding-top="4px" padding-bottom="6px" on-background="true"><b>${this.getContent()}</b></labor-adobe-typo-headingfive>
-                </mj-column>
-            </labor-adobe-section>
-        `)
+        return this.renderMJML(`<labor-adobe-footer-band padding-top="4px" padding-bottom="6px">${this.getContent()}</labor-adobe-footer-band>`)
     }
 }
